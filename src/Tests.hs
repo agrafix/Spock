b969a9a8fc3a2adf2ee569f835484fe393d1d70b
Alexander Thiemann <mail@agrafix.net>
2014-09-22 16:55:49 +0200

    switched from HTF to hspec
    this switch makes running the tests a lot easier because it drops the haskell-src-exts dependency.

--- src/Tests.hs
+++
@@ -1,9 +0,0 @@
-{-# OPTIONS_GHC -F -pgmF htfpp #-}
-module Main where
-
-import Test.Framework
-import {-@ HTF_TESTS @-} Tests.Routing
-import {-@ HTF_TESTS @-} Tests.Wire
-
-main :: IO ()
-main = htfMain htf_importedTests
