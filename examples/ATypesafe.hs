f1bd59b64a72da4181a3b2dd3a95beda25dabdbe
Alexander Thiemann <mail@athiemann.net>
2016-09-03 14:05:37 +0200

    remove broken example

--- examples/ATypesafe.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Web.Spock
-
-import qualified Data.Text as T
-
-main =
-    runSpock 3000 $ spockT id $
-    do get ("echo" <//> var) $ \something ->
-        text $ T.concat ["Echo: ", something]
