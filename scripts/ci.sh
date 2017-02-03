1d62f876a04ca34624cb0290e7cce4cfb7977955
Alexander Thiemann <mail@athiemann.net>
2017-02-03 20:43:06 +0100

    ci: next attempt to get travis working again

---
+++ scripts/ci.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+set -x
+
+step="$1"
+
+case "$step" in
+    install)
+        mkdir -p $HOME/.local/bin
+        case "$BUILD" in
+            stack)
+                stack $STACK_ARGS setup --no-terminal
+                if [ "$PUBLISH_DOCS" = "yes" ]; then
+                    stack $STACK_ARGS install --no-terminal hscolour
+                fi
+                stack $STACK_ARGS build --fast --only-snapshot --no-terminal --haddock
+                ;;
+            cabal)
+                sed -i 's/^jobs:/-- jobs:/' ${HOME}/.cabal/config;
+                cabal new-build --enable-tests -j4 --dep Spock Spock-core Spock-api Spock-api-server reroute
+                ;;
+        esac
+        ;;
+    script)
+        case "$BUILD" in
+            stack)
+                stack $STACK_ARGS $STACK_BUILD_MODE --pedantic --fast --no-terminal --skip-ghc-check $STACK_BUILD_ARGS
+                ;;
+            cabal)
+                cabal new-build --enable-tests -j4 Spock Spock-core Spock-api Spock-api-server reroute
+                ;;
+        esac
+        ;;
+    *)
+        echo "Bad step: $step"
+        exit 1
+        ;;
+esac
