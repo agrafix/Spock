354e32cb4ceade0028c5da20ec8518c7a065db9b
Alexander Thiemann <mail@athiemann.net>
2020-12-08 22:43:17 -0800

    adjust and bump

--- bump-version.sh
+++ bump-version.sh
@@ -3,4 +3,4 @@ VERSION="$1"
 
 echo "Bumping version to $VERSION ..."
 
-gfind . -name '*.cabal' -type f -not -path "./reroute/*" -exec sed -i '' "s/^version:.*/version:             $VERSION/g" {} +
+find . -name '*.cabal' -type f -not -path "./reroute/*" -exec sed -i '' "s/^version:.*/version:             $VERSION/g" {} +
